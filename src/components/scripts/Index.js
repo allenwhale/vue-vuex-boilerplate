f27e1fc6015bdfcb362d0b3a18e828e2e99bc3d5
allenwhale <allenwhale@yahoo.com.tw>
2016-09-04 01:40:36 +0800

    add example

--- src/components/scripts/Index.js
+++ src/components/scripts/Index.js
@@ -10,15 +10,37 @@ export default {
             exampleGet
         }
     },
-    data: {
+    props: {
+    },
+    data() {
+        return {
+            data1: 'this is data1'
+        }
     },
     computed: {
+        computedMsg () {
+            return this.example.msg + ' computed!'
+        }
     },
     methods: {
+        onClick () {
+            alert('On Click');
+        }
+    },
+    // life cycle
+    created () {
+    },
+    beforeCompile () {
+    },
+    compiled () {
     },
     ready () {
         this.changeMsg('hello! index')
         this.exampleGet().then(() => console.log('get done'));
-    }
+    },
+    beforeDestroy () {
+    },
+    destroyed () {
+    },
 }
 
